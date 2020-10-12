3aeef6894403d12b643497def24e4059f52e8962
Javier Botero <javierbotero10271@gmail.com>
2020-10-11 23:31:55 -0500

    Add funtionality of add and remove marks, also evaluate if there is a winner and display message of winner with an incremenet of the score

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,37 +1,139 @@
+const gameMatch = (() => {
+  const players = [];
+  const shift = '';
+  let playing = false;
+  const changeScore = (index) => {
+    score[index] += 1;
+  };
+  function playGame() {
+    // eslint-disable-next-line no-use-before-define
+    gameBoard.message.innerHTML = `${gameMatch.shift.namePlayer} make a move`;
+  }
+
+  function displayScore() {
+    const div = document.getElementById('score');
+    div.innerHTML = '<p class="col-12 text-center bg-info text-white m-0">SCORE<p>';
+    for (let i = 0; i < players.length; i += 1) {
+      div.innerHTML += `<div class="col-6 p-2 bg-info text-white text-center">
+                         <p>${players[i].namePlayer}</p>
+                         <p>${players[i].score}</p>
+                       </div>`;
+    }
+  }
+
+  return {
+    changeScore,
+    players,
+    shift,
+    playGame,
+    playing,
+    displayScore,
+  };
+})();
+
 const gameBoard = (() => {
-  const board = ['', '', '', '', '', '', '', '', ''];
+  let board = ['', '', '', '', '', '', '', '', ''];
   const tile = ['X', 'O'];
   const message = document.getElementById('message');
 
-  const mark = (space, tile) => {
-    if (board[space] !== "") {
-      message.innerHTML = "Choose another space";
-    } else {
-      board[space] = tile;
-      check();
+  function changeDivsColors() {
+    let indexes = [];
+    if (board[0] === board[1] && board[1] === board[2] && board[0] !== '') {
+      indexes = [0,1,2];
     }
+    if (board[3] === board[4] && board[4] === board[5] && board[3] !== '') {
+      indexes = [3,4,5];
+    }
+    if (board[6] === board[7] && board[7] === board[8] && board[6] !== '') {
+      indexes = [6,7,8];
+    }
+    if (board[0] === board[3] && board[3] === board[6] && board[0] !== '') {
+      indexes = [0,3,6];
+    }
+    if (board[1] === board[4] && board[4] === board[7] && board[1] !== '') {
+      indexes = [1,4,7];
+    }
+    if (board[2] === board[5] && board[5] === board[8] && board[2] !== '') {
+      indexes = [2,5,8];
+    }
+    if (board[0] === board[4] && board[4] === board[8] && board[0] !== '') {
+      indexes = [0,4,8];
+    }
+    if (board[2] === board[4] && board[4] === board[6] && board[2] !== '') {
+      indexes = [2,4,6];
+    }
+
+    for (let i = 0; i < indexes.length; i += 1) {
+      document.getElementById(indexes[i]).className += ' bg-success';
+    }
+  }
+
+  const resetBoard = () => {
+    // eslint-disable-next-line no-use-before-define
+    board = ['', '', '', '', '', '', '', '', ''];
+    gameMatch.playing = true;
+    console.log(gameMatch.playing);
+    // eslint-disable-next-line no-use-before-define
+    displayBoard();
+    // eslint-disable-next-line no-use-before-define
+    const reset = document.getElementById('reset');
+    const button = document.getElementById('button-reset');
+    reset.removeChild(button);
+    gameMatch.playGame();
   };
 
-  const check = (player) => {
-    if (
-      (board[0] === board[1] && board[1] === board[2]) ||
-      (board[3] === board[4] && board[4] === board[5]) ||
-      (board[6] === board[7] && board[7] === board[8]) ||
-      (board[0] === board[3] && board[3] === board[6]) ||
-      (board[1] === board[4] && board[4] === board[7]) ||
-      (board[2] === board[5] && board[5] === board[8]) ||
-      (board[0] === board[4] && board[4] === board[8]) ||
-      (board[2] === board[4] && board[4] === board[6])
-    ) {
-      return `${player} won!!`;
-    }
-  };
+  function displayReset() {
+    const reset = document.getElementById('reset');
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.setAttribute('id', 'button-reset');
+    button.addEventListener('click', () => { resetBoard(); });
+    button.innerHTML = 'Reset';
+    reset.appendChild(button);
+  }
 
   const displayBoard = () => {
     const container = document.getElementById('container');
     container.innerHTML = '';
     for (let i = 0; i < 9; i += 1) {
-      container.innerHTML += `<div class="spot border border-warning d-flex justify-content-center align-items-center text-white font-weight-bold">${board[i]}</div>`;
+      container.innerHTML += `<div data-number="${i}" id="${i}" class="spot border border-warning d-flex justify-content-center align-items-center text-white font-weight-bold">${board[i]}</div>`;
+    }
+  };
+
+  const check = (player) => {
+    if (
+      (board[0] === board[1] && board[1] === board[2] && board[0] !== '')
+      || (board[3] === board[4] && board[4] === board[5] && board[3] !== '')
+      || (board[6] === board[7] && board[7] === board[8] && board[6] !== '')
+      || (board[0] === board[3] && board[3] === board[6] && board[0] !== '')
+      || (board[1] === board[4] && board[4] === board[7] && board[1] !== '')
+      || (board[2] === board[5] && board[5] === board[8] && board[2] !== '')
+      || (board[0] === board[4] && board[4] === board[8] && board[0] !== '')
+      || (board[2] === board[4] && board[4] === board[6] && board[2] !== '')
+    ) {
+      message.innerHTML = `${player.namePlayer} won!!`;
+      player.score += 1;
+      gameMatch.playing = false;
+      gameMatch.displayScore();
+      changeDivsColors();
+      displayReset();
+    } else {
+      gameMatch.shift = gameMatch.shift == gameMatch.players[0] ? gameMatch.players[1] : gameMatch.players[0];
+      gameMatch.playGame();
+    }
+  };
+
+  const mark = (tag) => {
+    if (!gameMatch.playing) {
+      return;
+    }
+
+    if (tag.innerHTML !== '') {
+      message.innerHTML = 'Choose another space';
+    } else {
+      board[tag.dataset.number] = gameMatch.shift.markPlayer;
+      tag.innerHTML = board[tag.dataset.number];
+      check(gameMatch.shift);
     }
   };
 
@@ -40,55 +142,24 @@ const gameBoard = (() => {
     check,
     displayBoard,
     tile,
+    message,
+    resetBoard,
   };
 })();
 
 const player = (name, mark) => {
   const namePlayer = name;
   const markPlayer = mark;
+  // eslint-disable-next-line prefer-const
+  let score = 0;
   const getName = () => namePlayer;
-  const selectTile = (tile) => {
-    const message = document.createElement('div');
-    message.innerHTML = "Select a tile by selecting 0 for 'O' and 1 for 'X'";
-    let input = document.getElementById("index");
-    index = input.value;
-    if (index === 0) {
-      player.tile = tile.unshift;
-    } else {
-      player.tile = tile.pop;
-    }
-  };
 
   return {
     namePlayer,
     markPlayer,
     getName,
-    selectTile,
+    score,
   };
 };
 
-const game = (() => {
-  let score = [0, 0];
-  const players = [];
-  let shift = '';
-  const changeScore = (index) => {
-    score[index] += 1;
-  };
-  const resetBoard = () => {
-    gameBoard.board = ['', '', '', '', '', '', '', '', ''];
-  }
-  const resetScore = () => {
-    score = [0, 0];
-  }
-
-  return {
-    score,
-    changeScore,
-    resetBoard,
-    resetScore,
-    players,
-    shift,
-  }
-})();
-
-export { gameBoard, player, game };
+export { gameBoard, player, gameMatch };
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,34 +8,43 @@ function welcome() {
   game.gameBoard.displayBoard();
 }
 
+function startGame() {
+  game.gameMatch.playing = true;
+  game.gameBoard.board = ['', '', '', '', '', '', '', '', ''];
+  game.gameBoard.displayBoard();
+  const spots = document.getElementsByClassName('spot');
+  for (let i = 0; i < spots.length; i += 1) {
+    spots[i].addEventListener('click', () => { game.gameBoard.mark(spots[i]); });
+  }
+  game.gameMatch.displayScore();
+  game.gameMatch.playGame(game.gameMatch.players[0]);
+}
+
 function selectPlayer() {
   message.innerHTML = `Player ${3 - game.gameBoard.tile.length} write a name and select a mark<br>
                        <input type="text" placeholder="Name" id="player-name"></input>`;
   const options = document.createElement('ul');
   for (let i = 0; i < game.gameBoard.tile.length; i += 1) {
-    options.innerHTML += `<li onclick="logic.createPlayer(${game.gameBoard.tile[i]})" class="x list-inline-item p-3 rounded-circle text-white">${game.gameBoard.tile[i]}</li>`;
+    options.innerHTML += `<li id="${game.gameBoard.tile[i]}" class="mark-options list-inline-item p-3 rounded-circle text-white">${game.gameBoard.tile[i]}</li>`;
+  }
+  message.appendChild(options);
+  const optionsMark = document.getElementsByClassName('mark-options');
+  for (let i = 0; i < optionsMark.length; i += 1) {
+    optionsMark[i].addEventListener('click', createPlayer);
   }
 }
 
-function startGame() {
-  game.gameBoard.board = ['', '', '', '', '', '', '', '', ''];
-  game.gameBoard.displayBoard();
-  playGame(game.game.players[0]);
-}
-
-function playGame(player) {
-  game.gameBoard.message.innerHTML = `${player.name} make a move`;
-  game.game
-}
-
-function createPlayer(mark) {
+function createPlayer() {
   const playerName = document.getElementById('player-name').value;
-  game.gameBoard.players.push(game.player(playerName, mark));
+  const player = game.player(playerName, this.innerHTML);
+  game.gameMatch.players.push(player);
+  game.gameBoard.tile.splice(game.gameBoard.tile.findIndex(mark => mark === player.markPlayer), 1);
   if (game.gameBoard.tile.length > 0) {
     selectPlayer();
   } else {
+    game.gameMatch.shift = game.gameMatch.players[0];
     startGame();
   }
 }
 
-export { welcome, selectPlayer, createPlayer }
+export { welcome, selectPlayer, createPlayer };
